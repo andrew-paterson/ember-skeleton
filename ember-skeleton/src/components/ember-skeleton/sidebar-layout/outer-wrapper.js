02f4105169df76218f7baa97dd2d80908cf72375
Andrew Paterson <andrew.paterson9@gmail.com>
2025-01-24 10:22:53 +0200

    Continue with styles

--- ember-skeleton/src/components/ember-skeleton/sidebar-layout/outer-wrapper.js
+++ ember-skeleton/src/components/ember-skeleton/sidebar-layout/outer-wrapper.js
@@ -1,6 +1,6 @@
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
-import './ember-skeleton.css';
+// import './ember-skeleton.css';
 
 export default class OuterWrapper extends Component {
   @service
